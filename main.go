3f29ddab94a2315d0dcffcfd36c9711e4b35145c
tsingroo <tsingroo@gmail.com>
2023-02-18 18:56:55 +0800

    update

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	gpt3controller "go-chatgpt/gpt3/controller"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
+	engine.LoadHTMLGlob("templates/*")
+
+	engine.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{})
+	})
 	engine.GET("/stream", gpt3controller.HandleStreamCompletion)
 
-	engine.Run(":8080")
+	engine.Run(":8090")
 }
